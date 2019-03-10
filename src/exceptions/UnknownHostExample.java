8a1ee8158b8c38c04b44db1e86f67a0766b42e77
wesleyramos <wesleyramos523@gmail.com>
2019-03-10 09:06:58 -0300

    Added UnknownHost example

---
+++ src/exceptions/UnknownHostExample.java
@@ -0,0 +1,30 @@
+package exceptions;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
+import org.apache.log4j.Logger;
+
+public class UnknownHostExample {
+
+  private static final Logger LOGGER = Logger.getLogger(UnknownHostExample.class);
+
+  public static void main(String[] args) {
+    try {
+      String hostname = "www.google.com";
+      InetAddress address = InetAddress.getByName(hostname);
+      System.out.println(address.getHostName());
+    } catch (UnknownHostException e) {
+      LOGGER.error(e);
+    }
+
+    try {
+      String hostname = "www.notexisthostname.com";
+      InetAddress address = InetAddress.getByName(hostname);
+      System.out.println(address.getHostName());
+    } catch (UnknownHostException e) {
+      LOGGER.error(e);
+    }
+  }
+
+}
